ef626fe9182512db2bed1bea7036f57dbd4ca8c6
Juan José Fernández Núñez <62987728+jjfernunez@users.noreply.github.com>
2022-10-15 16:52:15 +0200

    Delete src directory

--- src/Controlador/ESaldoNoValido.java
+++
@@ -1,8 +0,0 @@
-package Controlador;
-
-public class ESaldoNoValido extends Exception {        
-    public ESaldoNoValido() {}
-    public ESaldoNoValido(String mensaje) {
-        super(mensaje);
-    };    
-}
